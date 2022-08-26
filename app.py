76bfdd9193b0be94849a312148082b36673bb682
James Moulds <jimmymoulds@gmail.com>
2022-08-26 17:20:14 +0100

    initial webapp files

---
+++ app.py
@@ -0,0 +1,80 @@
+# In this file we'll build our web app to investigate bitcoin volatility
+
+
+import streamlit as st
+import datetime
+from datetime import timedelta
+from plotly.subplots import make_subplots
+import talib
+import requests
+import matplotlib.pyplot as plt
+from volatility_funcs import *
+import pandas as pd
+import plotly.graph_objects as go
+import numpy as np
+
+# Set page in wide mode
+st.set_page_config(layout="wide")
+# Read in the base dataset - exported 17th Aug 2022
+btc = pd.read_csv("1D170822tv.csv")
+# Clean the data - from function import - format and clean
+btc = data_clean(btc)
+# Get the latest update - call in latest (post 17-08-22) data
+btc_update = get_latest_data(btc)
+# Append latest to historical
+df = btc.append(btc_update)
+# add the volatility feature - percentage difference upper to lower bollinger band 20period/close/2std
+df["volatility"] = 1 - (df.lower / df.upper)
+
+# Create a feature to tell the plot what on-hover text to display
+hover_text = []
+for ind, row in df.iterrows():
+
+    hover_text.append(f"Date:{row.time}<br>"+
+                      f"Volatility: {round(row.volatility,3)}<br>"+
+                      f"$BTC: ${round(row.close)}"
+                     )
+df["text"] = hover_text
+
+# Isolate the required columns
+df = df[["time", "open", "high", "low", "close",
+                         "volume", "basis", "upper", "lower", "volatility", "text"]]
+
+# Note that when plotting the lower bound in plotly there are some infinite / nan log values returned
+# in order to avoid this we need to remove any negative lower bound values
+c = np.where(df['lower'] < 0 , 0.03, df["lower"])
+df["lower"] = c
+
+
+
+def main(df):
+
+    st.title("VOLATILITY TEST")
+    print(df.volatility.mean())
+
+    # Set the user Input
+    vol_select = st.sidebar.number_input(label="Enter a volatility threshold between 0.03-0.2", min_value=0.029, max_value=0.199, step=0.001)
+    period_select = st.sidebar.number_input(label="Enter a returns period between 20 and 365 days", min_value=20, max_value=365)
+
+    indicator_select = st.sidebar.radio(label="Select a confirmation indicator", options=["SMA", "EMA", "STOCHRSI", "MACD"])
+
+
+    print(f"DF: {df.shape}")
+
+    # Get the plotting data for volatility and timeperiod as specified by user
+    plot_vol = volatility_backtest(df=df, vol=vol_select, timeperiod=period_select)
+
+    df = build_indicator(df=df, indicator=indicator_select)
+
+    fig = plot_data(df=df, plot_vol=plot_vol, vol=vol_select, timeperiod=period_select, indicator=indicator_select)
+
+    st.plotly_chart(fig)
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main(df=df)
---
+++ volatility_funcs.py
@@ -0,0 +1,458 @@
+# Functions associated with btc volatility web app
+
+
+import pandas as pd
+import datetime
+from datetime import timedelta
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import talib
+import requests
+import warnings
+warnings.filterwarnings("ignore")
+
+
+# Function to clean the read in historical data
+def data_clean(btc):
+    # Base clean of data - isolate columns, get datetime format, remove nans etc
+    cols = ['time', 'open', 'high', 'low', 'close', 'Volume', 'Basis', 'Upper', 'Lower']
+    btc = btc[cols]
+    btc.time = pd.to_datetime(btc["time"], unit='s')
+    btc.dropna(axis=0, inplace=True)
+    # Change feature headers to suit standard
+    btc.rename(columns={"Volume": "volume", "Basis": "basis", "Upper": "upper", "Lower": "lower"}, inplace=True)
+
+    return btc
+
+def get_latest_data(btc):
+    #### USE COINBASE API TO CALL IN LATEST DATA
+
+    # Get the url and params
+    apiUrl = "https://api.pro.coinbase.com"
+    sym = "BTC-USD"
+    # Set the granularity - we want daily so 86400 seconds per day
+    barSize = "86400"
+    timeEnd = datetime.datetime.now()
+    delta = timedelta(days=100)
+    timeStart = timeEnd - (1 * delta)
+
+    parameters = {
+        "start": timeStart,
+        "end": timeEnd,
+        "granularity": barSize
+
+    }
+
+    # CALL IN THE DATA
+    data = requests.get(f"{apiUrl}/products/{sym}/candles",
+                        params=parameters,
+                        headers={"content-type": "application/json"}
+
+                        )
+
+    # SET INTO DF AND Reformat time
+    btc_cbs = pd.DataFrame(data.json(), columns=["time", "low", "high", "open", "close", "volume"])
+
+    btc_cbs["time"] = pd.to_datetime(btc_cbs["time"], unit="s")
+
+    # Invert ( to most recent at tail)
+    btc_latest = btc_cbs.iloc[::-1]
+    btc_latest.reset_index(inplace=True, drop=True)
+    btc_latest.tail()
+
+    # Buil latest bbs
+    bollies = talib.BBANDS(btc_latest["close"], timeperiod=20, nbdevup=2, nbdevdn=2)
+
+    bollies = pd.DataFrame(bollies).T.rename(columns={0: "upper", 1: "basis", 2: "lower"})
+
+    # APPEND bollinger band data to latest btc read in
+    btc_bb = btc_latest.join(bollies)
+
+    # Now isolate only the dates that we need updates for to speed up the code
+    btc_update = btc_bb[btc_bb.time.between(btc.iloc[-1].time, btc_bb.iloc[-2].time)].iloc[1:]
+
+
+    return btc_update
+
+
+
+# Run the volatility backtest
+def volatility_backtest(df, vol=0.2, timeperiod=100):
+    data_cols = ["thresh_date", "price", "gain", "fall", "volatility", "period"]
+
+    vol_data = pd.DataFrame(columns=data_cols)
+
+    # vol = float(input("What volatility level to check?"))
+    #
+    # timeperiod = input("Over what time period to check performance")
+
+    # Get the dataframe of all volatility values less than the user input
+    vol_df = df[df.volatility < vol]
+    print(f"VOL DF COLS: {vol_df.columns, vol_df.shape}")
+
+
+    # vol_df["month"] = vol_df.index.month
+    # vol_df["year"] = vol_df.index.year
+    # vol_df["month"] = vol_df.time.month
+    # vol_df["year"] = vol_df.time.year
+
+    unq = []
+    for ind, row in vol_df.iterrows():
+        # month = ind.month
+        # year = ind.year
+        month = row.time.month
+        year = row.time.year
+
+        unq.append((year, month))
+    vol_df["unique"] = unq
+    print(f"UNIQUE: {unq}")
+    list_of_dates = vol_df.unique.unique()
+    print(f"LIST {list_of_dates[:6], len(list_of_dates)}")
+
+    for i, vol_date in enumerate(list_of_dates):
+        high = 0
+        low = 0
+
+        # Get the start point from which to measure performance
+        start_date = vol_df[(vol_df.unique == vol_date) & (vol_df.volatility <= vol)].iloc[0].time
+
+        #     vol_df[vol_df.volatility<0.06].iloc[0].name+timedelta(days=10)
+
+        end_date = start_date + timedelta(days=int(timeperiod))
+        #     print(start_date.date(), end_date.date())
+        # Get the check period based on user input
+        #     check_period = btc.iloc[start_date.date():end_date.date()]
+        check_period = df[df.time.between(start_date, end_date)]
+        start_price = round(check_period.iloc[0].close, 3)
+
+        # Now iterate over the specified time period and check the max percentage gain and fall
+        for ind, row in check_period.iterrows():
+
+            new_low = row.low
+            new_high = row.high
+            pct_low = round(((new_low / start_price) - 1) * 100, 3)
+            pct_high = round(((new_high / start_price) - 1) * 100, 3)
+            if new_low < start_price and pct_low < low:
+                low = pct_low
+            elif new_high > start_price and pct_high > high:
+                high = pct_high
+
+        #         print(f"Threshold breached on {start_date}, at ${start_price}, after {timeperiod} days:")
+        #         print(f"Best Gain: {high}%, Best Fall: {low} \n")
+
+        data_point = pd.DataFrame({"thresh_date": start_date,
+                                   "price": start_price,
+                                   "gain": high,
+                                   "fall": low,
+                                   "volatility": vol,
+                                   "period": timeperiod}, index=[i])
+        vol_data = vol_data.append(data_point)
+
+    # Get a list of df indices to be added to the plot
+    plot_ind = []
+    # get the last accepted plot data
+    last_plot_date = 0
+    for ind, row in vol_data.iterrows():
+        if ind == 0:
+            plot_ind.append(ind)
+            last_plot_date = row.thresh_date
+
+        # If the current row is within a month of our last plot date - ignore it
+        elif row.thresh_date < last_plot_date + timedelta(days=30):
+            pass
+        # Else keep the plot date and add its index to the plot list
+        else:
+            plot_ind.append(ind)
+            last_plot_date = row.thresh_date
+
+    plot_vol = vol_data.loc[plot_ind]
+
+    # Add a feature to state whether the best trade was a buy or a sell - based on magnitude of price change
+    plot_vol["direction"] = plot_vol.apply(lambda x: "buy" if x.gain > abs(x.fall) else "sell", axis=1)
+
+    # Create the on hover information
+    # Add hover text
+    hover_text = []
+
+    for ind, row in plot_vol.iterrows():
+        #     print(row["volatility"])
+        hover_text.append(f"Date:{row.thresh_date}<br>" +
+                          f"Volatility: {round(row['volatility'], 3)}<br>" +
+                          f"Start Price: ${round(row.price)}<br>" +
+                          f"%age {row.period} day gain: {row.gain}<br>" +
+                          f"%age {row.period} day fall: {row.fall}"
+
+                          )
+    plot_vol["text"] = hover_text
+
+    return plot_vol
+
+
+# Build a confirmation indicator
+def build_indicator(df, indicator):
+
+    """
+    In this function we'll construct features that respresent a variety of confirmation indicators including:
+    1. SMA (200,100,50)
+    2. EMA (?)
+    3. MACD
+    4. STOCHRSI
+    """
+
+    # If indicator matches one of our expected then build that feature
+    if indicator=="SMA":
+
+        df["sma200"] = talib.SMA(df["close"], timeperiod=200)
+        df["sma100"] = talib.SMA(df["close"], timeperiod=100)
+        df["sma50"] = talib.SMA(df["close"], timeperiod=50)
+        # df["sma20"] = talib.SMA(df["close"], timeperiod=20)
+
+    elif indicator=="EMA":
+        df["ema21"] = talib.EMA(df["close"], timeperiod=21)
+        df["ema50"] = talib.EMA(df["close"], timeperiod=50)
+        df["ema200"] = talib.EMA(df["close"], timeperiod=200)
+
+    elif indicator=="STOCHRSI":
+        df["fastk"], df["fastd"] = talib.STOCHRSI(df["close"], timeperiod=14, fastk_period=3,
+                                                  fastd_period=3, fastd_matype=0)
+
+    # elif indicator=="MACD":
+
+    return df
+
+
+
+# Build the plotter
+def plot_data(df, plot_vol, vol, timeperiod, indicator):
+    # SET THE OVERALL FIGURE
+    # fig = make_subplots(rows=2, cols=1, subplot_titles=("Signals", "Confirmations"))
+    fig = make_subplots(rows=2, cols=1)
+    fig.update_layout(
+        width=1250,
+        height=1250,
+        yaxis_domain=[0.5, 0.99],
+        yaxis2_domain=[0.0, 0.47],  # Changes the aspect ratio of the second plot
+
+    )
+
+    # Hard code the halving dates as we want to display these on the visuals
+    halvings = ['2012-11-28', '2016-07-09', '2020-05-11']
+
+    # Get the user generated sell and buy signal data
+    sells_df = plot_vol[plot_vol.direction == "sell"]
+    buys_df = plot_vol[plot_vol.direction == "buy"]
+
+    # Build the first figure
+    # BTC USD
+    fig1 = go.Figure(
+        go.Line(x=df.time, y=df.close, line_width=2, name='$BTC', text=df["text"], hoverinfo='text')
+    )
+
+    # BTC LOWER BB
+    fig1.add_trace(
+        go.Line(x=df.time, y=df.lower, line_width=1, name="Lower Bound", text=df["text"], hoverinfo='text')
+    )
+
+    # BTC UPPER BB
+    fig1.add_trace(
+        go.Line(x=df.time, y=df.upper, line_width=1, name="Upper Bound", text=df["text"], hoverinfo='text')
+    )
+
+    # Add volatility trace to the main chart
+    # Use add_trace function and specify secondary_y axes = True.
+    fig1.add_trace(
+        go.Line(x=df.time, y=df.volatility, name="Volatility", yaxis="y2", opacity=0.4)
+    )
+
+    # Add sell signals
+    fig1.add_trace(
+        go.Scatter(mode="markers", x=sells_df.thresh_date, y=sells_df.price * 1.1, name="Sell Signal",
+                   marker=dict(
+                       symbol="arrow-down",
+                       color="red",
+                       size=10
+                   ), text=sells_df.text, hoverinfo="text"))
+
+    # Add buy signals
+    fig1.add_trace(
+        go.Scatter(mode="markers", x=buys_df.thresh_date, y=buys_df.price * 0.9, name="Buy Signal",
+                   marker=dict(
+                       symbol="arrow-up",
+                       color="MidnightBlue",
+                       size=10
+                   ), text=buys_df.text, hoverinfo="text"))
+
+    # Add halvings
+    for i, line in enumerate(halvings):
+        if i == 0:
+            fig1.add_trace(
+                go.Line(x=[pd.to_datetime(line), pd.to_datetime(line)], y=[0.02, 95000],
+                        mode="lines", line={'dash': "dot", 'color': 'FireBrick', 'width': 1}, name="Halving")
+
+            )
+        else:
+            fig1.add_trace(
+                go.Line(x=[pd.to_datetime(line), pd.to_datetime(line)], y=[0.02, 95000],
+                        mode="lines", line={'dash': "dot", 'color': 'FireBrick', 'width': 1}, showlegend=False)
+
+            )
+
+    # Add traces to figure
+    fig.add_trace(fig1["data"][0], row=1, col=1)
+    fig.add_trace(fig1["data"][1], row=1, col=1)
+    fig.add_trace(fig1["data"][2], row=1, col=1)
+    fig.add_trace(fig1["data"][3], row=1, col=1)
+    fig.add_trace(fig1["data"][4], row=1, col=1)
+    fig.add_trace(fig1["data"][5], row=1, col=1)
+    fig.add_trace(fig1["data"][6], row=1, col=1)
+    fig.add_trace(fig1["data"][7], row=1, col=1)
+    fig.add_trace(fig1["data"][8], row=1, col=1)
+
+    # PLOT THE SECONDARY INDICATOR
+    fig2 = go.Figure()
+    if indicator=="SMA":
+        fig2.add_trace(
+            go.Line(x=df.time, y=df.sma200, name="SMA_200")
+
+        )
+        fig2.add_trace(
+            go.Line(x=df.time, y=df.sma100, name="SMA_100")
+
+        )
+        fig2.add_trace(
+            go.Line(x=df.time, y=df.sma50, name="SMA_50")
+
+        )
+
+        # Add sell signals
+        fig2.add_trace(
+            go.Scatter(mode="markers", x=sells_df.thresh_date, y=sells_df.price*1.1, name="Sell Signal",
+                       marker=dict(
+                           symbol="arrow-down",
+                           color="red",
+                           size=10
+                       ), showlegend=False))
+
+        # Add buy signals
+        fig2.add_trace(
+            go.Scatter(mode="markers", x=buys_df.thresh_date, y=buys_df.price*0.9, name="Buy Signal",
+                       marker=dict(
+                           symbol="arrow-up",
+                           color="MidnightBlue",
+                           size=10
+                       ), showlegend=False))
+
+        fig.add_trace(fig2["data"][0], row=2, col=1)
+        fig.add_trace(fig2["data"][1], row=2, col=1)
+        fig.add_trace(fig2["data"][2], row=2, col=1)
+        fig.add_trace(fig2["data"][3], row=2, col=1)
+        fig.add_trace(fig2["data"][4], row=2, col=1)
+
+    elif indicator=="STOCHRSI":
+        fig2.add_trace(
+            go.Line(x=df.time, y=df.fastk, name="STOCH_RSI_K")
+
+        )
+        fig2.add_trace(
+            go.Line(x=df.time, y=df.fastd, name="STOCH_RSI_D")
+
+        )
+
+        # Add sell signals
+        fig2.add_trace(
+            go.Scatter(mode="markers", x=sells_df.thresh_date, y=sells_df.price, name="Sell Signal",
+                       marker=dict(
+                           symbol="arrow-down",
+                           color="red",
+                           size=10
+                       ), showlegend=False))
+
+        # Add buy signals
+        fig2.add_trace(
+            go.Scatter(mode="markers", x=buys_df.thresh_date, y=buys_df.price, name="Buy Signal",
+                       marker=dict(
+                           symbol="arrow-up",
+                           color="MidnightBlue",
+                           size=10
+                       ), showlegend=False))
+
+        fig.add_trace(fig2["data"][0], row=2, col=1)
+        fig.add_trace(fig2["data"][1], row=2, col=1)
+        fig.add_trace(fig2["data"][2], row=2, col=1)
+        fig.add_trace(fig2["data"][3], row=2, col=1)
+
+    elif indicator=="EMA":
+
+        fig2.add_trace(
+            go.Line(x=df.time, y=df.ema200, name="EMA_200")
+
+        )
+        fig2.add_trace(
+            go.Line(x=df.time, y=df.ema50, name="EMA_50")
+
+        )
+        fig2.add_trace(
+            go.Line(x=df.time, y=df.ema21, name="EMA_21")
+
+        )
+
+        # Add sell signals
+        fig2.add_trace(
+            go.Scatter(mode="markers", x=sells_df.thresh_date, y=sells_df.price * 1.1, name="Sell Signal",
+                       marker=dict(
+                           symbol="arrow-down",
+                           color="red",
+                           size=10
+                       ), showlegend=False))
+
+        # Add buy signals
+        fig2.add_trace(
+            go.Scatter(mode="markers", x=buys_df.thresh_date, y=buys_df.price * 0.9, name="Buy Signal",
+                       marker=dict(
+                           symbol="arrow-up",
+                           color="MidnightBlue",
+                           size=10
+                       ), showlegend=False))
+
+        fig.add_trace(fig2["data"][0], row=2, col=1)
+        fig.add_trace(fig2["data"][1], row=2, col=1)
+        fig.add_trace(fig2["data"][2], row=2, col=1)
+        fig.add_trace(fig2["data"][3], row=2, col=1)
+        fig.add_trace(fig2["data"][4], row=2, col=1)
+
+
+
+
+
+
+    fig.update_layout(
+        xaxis2_title="TIME",
+        yaxis_title="BTCUSD 1D CLOSE",
+        yaxis2_title=f"{indicator}",
+        title=f"BITCOIN VOLATILITY - Signals volatility < {vol} over {timeperiod} days",
+
+    )
+
+    # Set the y axis to log type so we can more easily see the historic values
+    # fig.update_yaxes(type="log", row=1, col=1)
+    fig.update_yaxes(type="log")
+    fig.update_xaxes(showgrid=False)
+    # Add traces to figure
+    # fig.add_trace(fig1["data"][0], row=1, col=1)
+    # fig.add_trace(fig1["data"][1], row=1, col=1)
+    # fig.add_trace(fig1["data"][2], row=1, col=1)
+    # fig.add_trace(fig1["data"][3], row=1, col=1)
+    # fig.add_trace(fig1["data"][4], row=1, col=1)
+    # fig.add_trace(fig1["data"][5], row=1, col=1)
+    # fig.add_trace(fig1["data"][6], row=1, col=1)
+    # fig.add_trace(fig1["data"][7], row=1, col=1)
+    # fig.add_trace(fig1["data"][8], row=1, col=1)
+    # fig.add_trace(fig1["data"][9], row=1, col=1)
+    # fig.add_trace(fig2["data"][0], row=2, col=1)
+    # fig.add_trace(fig2["data"][1], row=2, col=1)
+    # fig.add_trace(fig2["data"][2], row=2, col=1)
+    # fig.add_trace(fig2["data"][3], row=2, col=1)
+    # fig.add_trace(fig2["data"][4], row=2, col=1)
+    # fig.show()
+
+    return fig
